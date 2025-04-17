ebe6cb83f14da5d4a0c911e79aca51e1fff7f112
Warmeson de Freitas Alencar <warmisonalencar@gmail.com>
2025-04-17 18:11:30 -0300

    .

--- ex4.php
+++ ex4.php
@@ -1,8 +1,8 @@
 <?php
 $numeros = [1, 2, 3, 4];
-array_push($numeros, 5); // Adiciona o número 5
-array_pop($numeros); // Remove o último elemento (5)
-print_r($numeros); // Exibe o array final
+array_push($numeros, 5); 
+array_pop($numeros);
+print_r($numeros);
 ?>
 
 
--- ex7.php
+++ ex7.php
@@ -2,8 +2,8 @@
 $numeros = [34, 12, 56, 78, 2, 45];
 $maior = max($numeros);
 $menor = min($numeros);
-echo "Maior valor: " . $maior . "<br>";
-echo "Menor valor: " . $menor;
+echo "Maior valor: " . $maior . "------";
+echo "Valor menor: " . $menor;
 ?>
 
 
